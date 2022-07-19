18a68db06ce7a3c2f463a382f4e95e48a2fc19e1
Raman Talwar <raman.talwar@ugent.be>
2022-07-19 13:47:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,30 +5,30 @@ import pandas as pd
 app = Flask(__name__)
 CORS(app)
 
-total_downloads=pd.read_csv("full_page_downloads_biblio.csv")
+# total_downloads=pd.read_csv("full_page_downloads_biblio.csv")
 
-df = pd.read_csv("publications.csv",low_memory=False)
-df=df[['id','jcr_eigenfactor',
-       'jcr_immediacy_index', 'jcr_impact_factor', 'jcr_impact_factor_5year',
-       'jcr_total_cites', 'jcr_category_quartile', 'jcr_prev_impact_factor',
-       'jcr_prev_category_quartile']]
+# df = pd.read_csv("publications.csv",low_memory=False)
+# df=df[['id','jcr_eigenfactor',
+#        'jcr_immediacy_index', 'jcr_impact_factor', 'jcr_impact_factor_5year',
+#        'jcr_total_cites', 'jcr_category_quartile', 'jcr_prev_impact_factor',
+#        'jcr_prev_category_quartile']]
 
 @app.route("/")
 def home():
-    print(df)
+    # print(df)
     return "Hello flask!"
 
 
 @app.route("/biblioMetrics/<bibid>")
 def getBiblioMetrics(bibid):
-    return df[df['id']==int(bibid)].set_index("id").to_json()
-
+    # return df[df['id']==int(bibid)].set_index("id").to_json()
+    return "hello"
 @app.route("/biblioDownloads/<bibid>")
 def getBiblioDownloads(bibid):
-    return total_downloads[total_downloads['id']==bibid].set_index("id").to_json()
+    # return total_downloads[total_downloads['id']==bibid].set_index("id").to_json()
 
     # return total_downloads.loc[[bibid]].to_json()
-
+    return "THis works"
 
 if __name__ == "__main__":
     app.run()
