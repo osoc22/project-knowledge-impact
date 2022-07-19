db7b9387924256c8bde46ebfd1a3641b7407f190
Raman Talwar <raman.talwar@ugent.be>
2022-07-19 15:32:21 +0200

    added work count and citation count plot

--- index.js
+++ index.js
@@ -1,54 +1,107 @@
 
-const loadPeopleWhoCitedMe = function () {//get publications from fris, 
-    fetch("")//seding a request with my doi and getting the persons as a reposnse
-        .then(function (response) {
+// const loadPeopleWhoCitedMe = function () {//get publications from fris, 
+//     fetch("")//seding a request with my doi and getting the persons as a reposnse
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 throw Error(`Problem with the fetch(). Status Code: ${response.status}`);
+//             } else {
+//                 // console.info('There is a response from the server');
+//                 return response.json();
+//             }
+//         })
+//         .then(function (jsonObject) {
+//             // console.info('a json object has been made');
+//             // console.info('percessing data');
+//             verwerkData(jsonObject)
+//         })
+//         .catch(function (error) {
+//             console.error(`error with processing json ${error}`);
+//         });
+// };
+
+// const loadPublications = function () {//get publications from fris, 
+//     fetch("")
+//         .then(function (response) {
+//             if (!response.ok) {
+//                 throw Error(`Problem with the fetch(). Status Code: ${response.status}`);
+//             } else {
+//                 // console.info('There is a response from the server');
+//                 return response.json();
+//             }
+//         })
+//         .then(function (jsonObject) {
+//             // console.info('a json object has been made');
+//             // console.info('percessing data');
+//             verwerkData(jsonObject)
+//         })
+//         .catch(function (error) {
+//             console.error(`error with processing json ${error}`);
+//         });
+// };
+
+
+function showCitationsAndWorkCount(data){
+    console.log(data.counts_by_year)
+    let years=[]
+    let workcounts=[]
+    let citations=[]
+    for(let obj of data.counts_by_year){
+        // console.log(obj)
+        years.push(obj.year)
+        workcounts.push(obj.works_count)
+        citations.push(obj.cited_by_count)
+    }
+    let plot = [
+        {
+          x: years,
+          y: workcounts,
+          type: 'scatter'
+        }
+      ];
+      
+      Plotly.newPlot('js_workCount', plot);
+      plot[0].y=citations
+      Plotly.newPlot('js_citations', plot);
+    
+
+}
+
+
+const loadCitationsAndWorkCount = function(orcid) {
+    fetch('http://api.openalex.org/authors/orcid:'+orcid)
+        .then(function(response) {
             if (!response.ok) {
-                throw Error(`Problem with the fetch(). Status Code: ${response.status}`);
+                throw Error(`Probleem bij de fetch(). Status Code: ${response.status}`);
             } else {
-                // console.info('There is a response from the server');
+                console.info('Er is een response teruggekomen van de server');
                 return response.json();
             }
         })
-        .then(function (jsonObject) {
-            // console.info('a json object has been made');
-            // console.info('percessing data');
-            verwerkData(jsonObject)
+        .then(function(jsonObject) {
+            console.info('json object is aangemaakt');
+            console.info('verwerken data');
+            showCitationsAndWorkCount(jsonObject)
         })
-        .catch(function (error) {
-            console.error(`error with processing json ${error}`);
+        .catch(function(error) {
+            console.error(`fout bij verwerken json error`);
         });
 };
-
-const loadPublications = function () {//get publications from fris, 
-    fetch("")
-        .then(function (response) {
-            if (!response.ok) {
-                throw Error(`Problem with the fetch(). Status Code: ${response.status}`);
-            } else {
-                // console.info('There is a response from the server');
-                return response.json();
-            }
-        })
-        .then(function (jsonObject) {
-            // console.info('a json object has been made');
-            // console.info('percessing data');
-            verwerkData(jsonObject)
-        })
-        .catch(function (error) {
-            console.error(`error with processing json ${error}`);
-        });
-};
-
 
 
 function searchORCID(e){
     ORCID=document.querySelector("#orcid").value
-    loadPublications()
+    // loadPublications()
+    loadCitationsAndWorkCount(ORCID)
 }
+
+
+document.querySelector("#searchbtn").addEventListener("click",searchORCID)
+
 
 start()
 
 
 function start(){
-    console.log("started")
+    console.log("started")    
+    // loadCitationsAndWorkCount("0000-0003-0248-0987")
 }
